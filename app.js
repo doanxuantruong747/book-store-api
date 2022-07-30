d50169d3ae3f01d983aa609cc9a7cc9c02ddb122
Truong <doanxuantruong747@gmail.com>
2022-07-30 08:20:08 +0700

    up

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+    const exception = new Error(`Path not found`)
+    exception.statusCode = 404;
+    next(exception)
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode).send(err.message)
+})
+
+module.exports = app;
